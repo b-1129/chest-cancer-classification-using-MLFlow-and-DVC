787552daa8966f964911b1b98f1b67a0b3b492e3
Brijesh Kapadiya <brijesh29.it11@gmail.com>
2025-03-04 22:38:42 +0530

    modified

--- src/cnnClassificationProject/pipeline/prediction.py
+++ src/cnnClassificationProject/pipeline/prediction.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 from keras.preprocessing import image
 from keras.models import load_model
+import tensorflow as tf
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
@@ -13,11 +14,12 @@ class PredictionPipeline:
     def predict(self):
         #load model
         # model = load_model(os.path.join("artifacts","training","model.h5"))
-        model = load_model(os.path.join("model", "model.h5"))
+        model_path = os.path.abspath(os.path.join("model", "model.h5"))
+        model = load_model(model_path)
 
         imagename = self.filename
-        test_image = image.load_img(imagename, target_size = (224,224))
-        test_image = image.img_to_array(test_image)
+        test_image = tf.keras.preprocessing.image.load_img(imagename, target_size = (224,224))
+        test_image = tf.keras.preprocessing.image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis=0)
         result = np.argmax(model.predict(test_image), axis=1)
         print(result)
